4ee1e62c03e8d04d7a35f6aaecfe462a42b6adcd
CaptainAarav <aaravsahni1037@gmail.com>
2026-07-28 22:03:59 +0530

    Added styling to hero section and deleted some files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
             <section className="hero-section">
                 <img src="/grandlux-logo.png" alt="logo" className="hero-logo" />
                 <h1 className="hero-title">GrandLux</h1>
-                <h2 className="hero-subtitle">The Luxembourgish way of flying with perfection</h2>
+                <h2 className="hero-subtitle"><em>The Luxembourgish way of flying with perfection</em></h2>
             </section>
         </>
     )
