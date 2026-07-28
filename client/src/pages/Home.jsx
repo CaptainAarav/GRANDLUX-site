9a085f79ce74744aaba3c510d9c997136aabc036
CaptainAarav <aaravsahni1037@gmail.com>
2026-07-28 22:46:30 +0530

    Added final styling to beginnings section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,6 +9,15 @@ function Home() {
                 <h1 className="hero-title">GrandLux</h1>
                 <h2 className="hero-subtitle"><em>The Luxembourgish way of flying with perfection</em></h2>
             </section>
+            <section className="beginnings-section">
+                <img src="/FLX-logo.png" alt="FLX logo" className="FLX-logo" />
+                <div className="beginnings-text-container">
+                    <h2 className="beginnigns-text-title">OUR BEGINNING</h2>
+                    <p className="beginnings-text">
+                        At first we wanted to launch the airline as FLX but that didn't really work out so we called it GRANDLUX. After a lot of work we got through it so here we are in our launching state stay tuned for updates on our nice VA.
+                    </p>
+                </div>
+            </section>
         </>
     )
 }
