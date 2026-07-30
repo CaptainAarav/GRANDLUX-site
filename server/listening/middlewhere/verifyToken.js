2cd2f96054379bf1fb08e24d14103082ee3d67cf
CaptainAarav <aaravsahni1037@gmail.com>
2026-07-30 17:01:30 +0530

    Finished verifyToken.js

---
+++ server/listening/middlewhere/verifyToken.js
@@ -0,0 +1,18 @@
+const admin = require('../firebase')
+
+async function verifyToken(req, res, next) {
+	const authHeader = req.headers.authorization
+	if (!authHeader || !authHeader.startsWith('Bearer ')) {
+		return res.status(401).json({ error: 'No token provided' })
+	}
+	const token = authHeader.split('Bearer ')[1]
+	try {
+		const decoded = await admin.auth().verifyIdToken(token)
+		req.pilotId = decoded.uid
+		next()
+	} catch (err) {
+		res.status(401).json({ error: 'Invalid token' })
+	}
+}
+
+module.exports = verifyToken
