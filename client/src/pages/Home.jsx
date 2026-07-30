2077f9ce99f5d8985a68749aed6df8d108445551
CaptainAarav <aaravsahni1037@gmail.com>
2026-07-31 01:25:33 +0530

    Added plane animation to hero page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,8 @@ function Home() {
     return (
         <>
             <section className="hero-section">
+                <img src="/grandlux-737-livery.png" className="hero-plane hero-plane-737" />
+                <img src="/a320-grandlux-livery.png" className="hero-plane hero-plane-a320" />
                 <img src="/grandlux-logo.png" alt="logo" className="hero-logo" />
                 <h1 className="hero-title">GrandLux</h1>
                 <h2 className="hero-subtitle">The Luxembourgish way of flying with perfection</h2>
