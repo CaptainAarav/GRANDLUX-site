8f067f52a8ee4d568f0f6c7fb1194cf76e8ab64b
CaptainAarav <aaravsahni1037@gmail.com>
2026-07-31 01:18:04 +0530

    Added plane liverys to sign up and get started page

--- client/src/pages/GetStarted.jsx
+++ client/src/pages/GetStarted.jsx
@@ -60,59 +60,67 @@ function GetStarted() {
 	return (
 		<div className="background">
 			<div className="modal">
-				<div className="title-container">
-					<img className="logo" src="/grandlux-logo.png" />
-					<h1 className="title">GrandLux</h1>
-				</div>
-				<h2 className="subtitle">Get Started</h2>
-				{error && <p className="login-error">{error}</p>}
+				<div className="modal-form">
+					<div className="title-container">
+						<img className="logo" src="/grandlux-logo.png" />
+						<h1 className="title">GrandLux</h1>
+					</div>
+					<h2 className="subtitle">Get Started</h2>
+					{error && <p className="login-error">{error}</p>}
 
-				<form onSubmit={handleSignupSubmit} className="auth-form">
-					<div className="name-inputs-container">
-						<div className="input-container">
-							<label htmlFor="first-name-input">First Name</label>
-							<input id="first-name-input" className="input-box" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="John" />
+					<form onSubmit={handleSignupSubmit} className="auth-form">
+						<div className="name-inputs-container">
+							<div className="input-container">
+								<label htmlFor="first-name-input">First Name</label>
+								<input id="first-name-input" className="input-box" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="John" />
+							</div>
+							<div className="input-container">
+								<label htmlFor="last-name-input">Last Name</label>
+								<input id="last-name-input" className="input-box" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Doe" />
+							</div>
 						</div>
 						<div className="input-container">
-							<label htmlFor="last-name-input">Last Name</label>
-							<input id="last-name-input" className="input-box" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Doe" />
+							<label htmlFor="email-input">Email Address</label>
+							<input id="email-input" className="input-box" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="your.email@example.com" />
 						</div>
-					</div>
-					<div className="input-container">
-						<label htmlFor="email-input">Email Address</label>
-						<input id="email-input" className="input-box" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="your.email@example.com" />
-					</div>
-					<div className="input-container">
-						<label htmlFor="password-input">Password</label>
-						<div className="password-input-wrapper">
-							<input value={password} onChange={e => setPassword(e.target.value)} id="password-input" className="input-box" type={showPassword ? "text" : "password"} placeholder="Create a password" />
-							<i className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowPassword(!showPassword)}></i>
+						<div className="input-container">
+							<label htmlFor="password-input">Password</label>
+							<div className="password-input-wrapper">
+								<input value={password} onChange={e => setPassword(e.target.value)} id="password-input" className="input-box" type={showPassword ? "text" : "password"} placeholder="Create a password" />
+								<i className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowPassword(!showPassword)}></i>
+							</div>
 						</div>
-					</div>
-					<div className="input-container">
-						<label htmlFor="confirm-password-input">Confirm Password</label>
-						<div className="password-input-wrapper">
-							<input value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} id="confirm-password-input" className="input-box" type={showConfirmPassword ? "text" : "password"} placeholder="Re-enter your password" />
-							<i className={`fa-solid ${showConfirmPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowConfirmPassword(!showConfirmPassword)}></i>
+						<div className="input-container">
+							<label htmlFor="confirm-password-input">Confirm Password</label>
+							<div className="password-input-wrapper">
+								<input value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} id="confirm-password-input" className="input-box" type={showConfirmPassword ? "text" : "password"} placeholder="Re-enter your password" />
+								<i className={`fa-solid ${showConfirmPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowConfirmPassword(!showConfirmPassword)}></i>
+							</div>
 						</div>
-					</div>
-					<div className="alignment-container">
-						<button type="submit" className="login-btn">Sign Up</button>
-					</div>
-				</form>
+						<div className="alignment-container">
+							<button type="submit" className="login-btn">Sign Up</button>
+						</div>
+					</form>
 
-				<div className="divider-container">
-					<div className="line"></div>
-					<p className="divider-text">or</p>
-					<div className="line"></div>
+					<div className="divider-container">
+						<div className="line"></div>
+						<p className="divider-text">or</p>
+						<div className="line"></div>
+					</div>
+
+					<button type="button" onClick={handleGoogleClick} className="google-btn">
+						<i className="fa-brands fa-google"></i>
+						<p className="btn-text">Sign up with Google</p>
+					</button>
+
+					<p>Already have an account? <NavLink className="sign-up-link" to="/login">Log in</NavLink></p>
 				</div>
-
-				<button type="button" onClick={handleGoogleClick} className="google-btn">
-					<i className="fa-brands fa-google"></i>
-					<p className="btn-text">Sign up with Google</p>
-				</button>
-
-				<p>Already have an account? <NavLink className="sign-up-link" to="/login">Log in</NavLink></p>
+				<div className="modal-liveries">
+					<div className="liveries-container">
+						<img src="/a320-grandlux-livery.png" className="livery-plane livery-a320" />
+						<img src="/grandlux-737-livery.png" className="livery-plane livery-737" />
+					</div>
+				</div>
 			</div>
 		</div>
 	)
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -61,49 +61,57 @@ function Login() {
 	return (
 		<div className="background">
 			<div className="modal">
-				<div className="title-container">
-					<img className="logo" src="/grandlux-logo.png" />
-					<h1 className="title">GrandLux</h1>
+				<div className="modal-form">
+					<div className="title-container">
+						<img className="logo" src="/grandlux-logo.png" />
+						<h1 className="title">GrandLux</h1>
+					</div>
+					<h2 className="subtitle">Welcome Back</h2>
+					{error && <p className="login-error">{error}</p>}
+
+					<form onSubmit={handleEmailSubmit} className="auth-form">
+						<div className="input-container">
+							<label htmlFor="email-input">Email Address</label>
+							<input id="email-input" className="input-box" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="your.email@example.com" />
+						</div>
+						<div className="input-container">
+							<div className="password-text-container">
+								<label htmlFor="password-input">Password</label>
+								<a className="forgot-password-link" href="">Forgot password?</a>
+							</div>
+							<div className="password-input-wrapper">
+								<input value={password} onChange={e => setPassword(e.target.value)} id="password-input" className="input-box" type={showPassword ? "text" : "password"} placeholder="Enter your password" />
+								<i className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowPassword(!showPassword)}></i>
+							</div>
+						</div>
+						<div className="alignment-container">
+							<div className="remember-me-container">
+								<input checked={rememberMe} onChange={e => setRememberMe(e.target.checked)} id="remember-me-radio" type="checkbox" className="remember-checkbox" />
+								<label htmlFor="remember-me-radio">Remember me</label>
+							</div>
+							<button type="submit" className="login-btn">Log In</button>
+						</div>
+					</form>
+
+					<div className="divider-container">
+						<div className="line"></div>
+						<p className="divider-text">or</p>
+						<div className="line"></div>
+					</div>
+
+					<button type="button" onClick={handleGoogleClick} className="google-btn">
+						<i className="fa-brands fa-google"></i>
+						<p className="btn-text">Sign in with Google</p>
+					</button>
+
+					<p>Is it your first time? <NavLink className="sign-up-link" to="/getstarted">Sign up</NavLink></p>
 				</div>
-				<h2 className="subtitle">Welcome Back</h2>
-				{error && <p className="login-error">{error}</p>}
-
-				<form onSubmit={handleEmailSubmit} className="auth-form">
-					<div className="input-container">
-						<label htmlFor="email-input">Email Address</label>
-						<input id="email-input" className="input-box" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="your.email@example.com" />
+				<div className="modal-liveries">
+					<div className="liveries-container">
+						<img src="/a320-grandlux-livery.png" className="livery-plane livery-a320" />
+						<img src="/grandlux-737-livery.png" className="livery-plane livery-737" />
 					</div>
-					<div className="input-container">
-						<div className="password-text-container">
-							<label htmlFor="password-input">Password</label>
-							<a className="forgot-password-link" href="">Forgot password?</a>
-						</div>
-						<div className="password-input-wrapper">
-							<input value={password} onChange={e => setPassword(e.target.value)} id="password-input" className="input-box" type={showPassword ? "text" : "password"} placeholder="Enter your password" />
-							<i className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} password-toggle-icon`} onClick={() => setShowPassword(!showPassword)}></i>
-						</div>
-					</div>
-					<div className="alignment-container">
-						<div className="remember-me-container">
-							<input checked={rememberMe} onChange={e => setRememberMe(e.target.checked)} id="remember-me-radio" type="checkbox" className="remember-checkbox" />
-							<label htmlFor="remember-me-radio">Remember me</label>
-						</div>
-						<button type="submit" className="login-btn">Log In</button>
-					</div>
-				</form>
-
-				<div className="divider-container">
-					<div className="line"></div>
-					<p className="divider-text">or</p>
-					<div className="line"></div>
 				</div>
-
-				<button type="button" onClick={handleGoogleClick} className="google-btn">
-					<i className="fa-brands fa-google"></i>
-					<p className="btn-text">Sign in with Google</p>
-				</button>
-
-				<p>Is it your first time? <NavLink className="sign-up-link" to="/getstarted">Sign up</NavLink></p>
 			</div>
 		</div>
 	)
